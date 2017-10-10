74cb4991df2fe0dfd69bf9516806570ee8cae8e2
jebyson <joshuaebyson@yahoo.com>
2017-10-10 08:44:40 -0400

    I think I'm done?!?

--- src/com/company/LuckyNumberGenerator.java
+++ src/com/company/LuckyNumberGenerator.java
@@ -3,9 +3,10 @@ package com.company;
 /**
  * Created by je565 on 10/10/17.
  */
+
 public class LuckyNumberGenerator {
     public int LuckyNumber;
-    public int luck = (int)(Math.random()*46340);
+    public int luck = (int)(Math.random()*46341);
 
     public LuckyNumberGenerator() {
     }
