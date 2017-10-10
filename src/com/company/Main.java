418143844978433ec47fdf42dc09cfbf10ed4407
jebyson <joshuaebyson@yahoo.com>
2017-10-10 08:43:00 -0400

    I think I'm done?!?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,25 +5,32 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = input.nextLine();
+        for(int j = 0; j <= 1; j += 0) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is your name?");
+            String name = input.nextLine();
 
-        int lucky = new LuckyNumberGenerator().perfectRandom();
-        System.out.println("Nice to meet you " + name + "! Your lucky number is " + lucky);
-        System.out.println("If you would like to exit, enter 0. If you would like a listing of your name and your lucky number, enter 1. If you would like to try again, enter 2. Thank you.");
+            int lucky = new LuckyNumberGenerator().perfectRandom();
+            System.out.println("Nice to meet you " + name + "! Your lucky number is " + lucky);
+            System.out.println("If you would like to exit, enter 0. If you would like a listing of your name and your lucky number, enter 1. If you would like to try again, enter 2. Thank you.");
 
-        Scanner nextInput = new Scanner(System.in);
-        int repeat = nextInput.nextInt();
+            Scanner nextInput = new Scanner(System.in);
+            int repeat = nextInput.nextInt();
 
-        if (repeat == 0) {
-            System.out.println("Goodbye!");
-        }
-        if (repeat == 1) {
-            System.out.println("Your name is " + name + " and your lucky number is " + lucky);
-        }
-        if (repeat == 2) {
-            
+            if (repeat == 0) {
+                System.out.println("Goodbye!");
+                break;
+            }
+            else if (repeat == 1) {
+                System.out.println("Your name is " + name + " and your lucky number is " + lucky);
+                break;
+            }
+            else if (repeat == 2) {
+            }
+            else {
+                System.out.println("Not a valid input. Goodbye");
+                break;
+            }
         }
     }
 }
